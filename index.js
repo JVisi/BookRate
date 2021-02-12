35516eb779f99bdab03517ab2aeabe16357a9500
Patrik Visi <justvisitf@gmail.com>
2021-02-12 11:38:17 +0100

    self signed cert trying stuff

--- index.js
+++ index.js
@@ -4,21 +4,17 @@ const cors = require("cors");
 const body_parser = require("body-parser");
 const passport=require('passport')
 const cookie=require("cookie-session")
+const fs =require('fs')
 require("./src/Authorization/PassportSetup")
 const router = require("./src/Routes/Router");
 const db=require('./src/DB/DBConfig');
-
-// const Rate = require("./src/DB/Entities/Rates");
-//  const Book = require("./src/DB/Entities/Book");
-//  const User = require("./src/DB/Entities/User");
-//  //User.sync({force:true})
-//  //Book.sync({force:true})
-//  Rate.sync()
-// db.testConnection()
-// Rate.findAll({ include: [Book,User], where: { id: 1 } }).then((result) => { console.log(result[0]); });
-
+const https=require('https')
 const port = process.env.port || 3000;
 const app = express();
+
+const key = fs.readFileSync('./cert/key.pem')
+const cert = fs.readFileSync('./cert/cert.pem')
+
 app.use(helmet());
 app.use(cors());
 app.use(cookie({
@@ -31,6 +27,10 @@ app.use(body_parser.json());
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(router)
-app.listen(port, () => {
-    console.info(`app is listening on port ${port}`);
-});
+
+app.get('/', (req, res) => { res.send('this is an secure server') });
+const server=https.createServer({key: key, cert: cert }, app)
+server.listen(port, () => { console.log(`listening on ${port}`) });
+// app.listen(port, () => {
+//     console.info(`app is listening on port ${port}`);
+// });
